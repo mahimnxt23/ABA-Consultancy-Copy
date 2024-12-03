c38c39b938b88581237b36977556ffcf23470508
mouEdn <mou@ethicalden.com>
2024-12-03 16:27:10 +0600

    contactUs gap fix

--- src/ClientSide/AboutUs/CollaborateWithU/CollaborateWithU.jsx
+++ src/ClientSide/AboutUs/CollaborateWithU/CollaborateWithU.jsx
@@ -1,16 +1,20 @@
 const CollaborateWithU = () => {
   return (
-    <div className="sectionGap">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-[30px] lg:gap-[100px] justify-center items-center">
-        <div className="overflow-hidden lg:h-[442px]  pb-[20px]   flex justify-center items-center z-10">
-          <img src="/images/Frame 101.svg" className="w-full h-full  " alt="" />
+    <div className="sectionGap ">
+      <div className="grid grid-cols-1 pb-[50px] lg:grid-cols-2 gap-[30px] lg:gap-[100px] justify-center items-center">
+        <div className=" overflow-hidden lg:h-[442px]  pb-[20px]   flex justify-center items-center z-10">
+          <img
+            src="/images/Frame 101.svg"
+            className="w-full h-full object-cover "
+            alt=""
+          />
         </div>
 
         <div>
-          <h1 className="headTwo text-[#0E0D0D] pb-[20px]">
+          <h1 className="headTwo text-[#0E0D0D] pb-[20px] ">
             We collaborate with you to tackle your key business priorities.
           </h1>
-          <p className="bodyText text-[#0E0D0DCC]/[.8]">
+          <p className="bodyText text-[#0E0D0DCC]/[.8] ">
             At ABA Counsaltant we specialize in providing tailored consultancy
             services to help businesses thrive in a competitive world. With
             years of experience and a passion for driving success, we focus on
