0fd0a51b9e5565926683a588a6afbe0732deee7b
mouEdn <mou@ethicalden.com>
2024-12-16 23:18:09 +0600

    choose us commit

--- src/ClientSide/AboutUs/AboutUs.jsx
+++ src/ClientSide/AboutUs/AboutUs.jsx
@@ -1,6 +1,5 @@
 import Footer from "../Shared/Footer/Footer";
 import AboutBanner from "./AboutBanner/AboutBanner";
-import ChooseUs from "./ChooseUs/ChooseUs";
 import CollaborateWithU from "./CollaborateWithU/CollaborateWithU";
 import Team from "./Team/Team";
 
@@ -8,7 +7,7 @@ const AboutUs = () => {
   return (
     <div className=" overflow-x-hidden">
       <AboutBanner></AboutBanner>
-      <ChooseUs></ChooseUs>
+      {/* <ChooseUs></ChooseUs> */}
       <CollaborateWithU></CollaborateWithU>
       <Team></Team>
       <div className="sectionGap bg-[#1a2633]">
