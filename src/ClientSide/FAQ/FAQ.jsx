53cb5713602a4b41383d96b713d348374e71bb60
mahim <mahimnxt23@gmail.com>
2025-01-02 17:40:24 +0600

    change on aba faq

---
+++ src/ClientSide/FAQ/FAQ.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react-refresh/only-export-components */
+import Faq from "../Service/Faq/Faq";
+import Footer from "../Shared/Footer/Footer";
+
+const FAQ = () => {
+	return (
+		<>
+			<Faq />
+			<div className="sectionGap bg-[#1a2633]">
+				<Footer></Footer>
+			</div>
+		</>
+	);
+};
+
+export default FAQ;
